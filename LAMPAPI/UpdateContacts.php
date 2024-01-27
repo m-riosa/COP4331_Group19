b08a3a615686ec3275009c977fbb255ee168bf36
M-Riosa <96079530+m-riosa@users.noreply.github.com>
2024-01-27 13:07:11 -0500

    Included specific error message if no match found

--- LAMPAPI/UpdateContacts.php
+++ LAMPAPI/UpdateContacts.php
@@ -14,12 +14,24 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE id=?");
-		$stmt->bind_param("sssss", $firstName, $lastName, $phone, $email, $id);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
+		$ret = $conn->prepare("SELECT FirstName FROM Contacts WHERE ID=?");
+	        $ret->bind_param("s", $id);
+	        $ret->execute();
+	        $ret->store_result();
+
+		if( $ret->num_rows > 0){
+			$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE id=?");
+			$stmt->bind_param("sssss", $firstName, $lastName, $phone, $email, $id);
+			$stmt->execute();
+			$stmt->close();
+			$conn->close();
+			returnWithError("");
+		}
+		else{
+			$ret->close();
+			$conn->close();
+			returnWithError("No Contact found with this ID");
+		}
 	}
 
     function getRequestInfo()
