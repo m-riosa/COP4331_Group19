588749619010b484c1df6f1deaeec84f99ee04be
M-Riosa <96079530+m-riosa@users.noreply.github.com>
2024-01-27 13:06:15 -0500

    Updated for login duplicate check

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -9,10 +9,23 @@
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
     if( $conn->connect_error )
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError( $conn->connect_error, 404);
 	}
 	else
 	{
+		$checkStmt = $conn->prepare("SELECT Login FROM Users WHERE Login = ?");
+		$checkStmt->bind_param("s", $login);
+		$checkStmt->execute();
+		$checkStmt->store_result();
+
+		if( $checkStmt->num_rows > 0 )
+		{
+			$checkStmt->close();
+			$conn->close();
+			returnWithError("Username already taken", 409);
+			return;
+		}
+
 		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
 		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password );
 		$stmt->execute();
@@ -32,8 +45,9 @@
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
+	function returnWithError( $err, $statusCode = 200)
 	{
+		http_response_code($statusCode);
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
